5270e48bdf2cf77e6ef46fa4fe130fa8a4e0af46
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-20 11:55:36 -0300

    Configurações iniciais do servidor backend para API.

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: 'application/json' }))
+
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send({message: `API ouvindo na porta ${PORT}` })
+
+})
+
+app.listen(PORT, () => console.log(`API ouvindo na porta ${PORT}`))
