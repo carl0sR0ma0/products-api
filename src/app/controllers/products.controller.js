79f23dc0da425bdacd7e54e6e5939bef60126be2
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-21 01:35:38 -0300

    Implementando e disponibilizando a rota de visualizarUm product.

--- src/app/controllers/products.controller.js
+++ src/app/controllers/products.controller.js
@@ -23,6 +23,18 @@ class Product {
       }
     })
   }
+
+  viewOneProduct(req, res) {
+    const name = req.params.nome
+
+    productSchema.find({name: name}, (err, data) => {
+      if (err) {
+        res.status(500).send({message: 'Houve um erro ao processar a sua requisição', error: err })
+      } else {
+        res.status(200).send({message: `Produto ${name} foi recuperado com sucesso`, product: data })
+      }
+    })
+  }
 }
 
 module.exports = new Product()
--- src/app/routes/products.routes.js
+++ src/app/routes/products.routes.js
@@ -4,5 +4,6 @@ const Product = require('./../controllers/products.controller')
 
 route.post('/criar', Product.createProduct)
 route.get('/visualizarTodos', Product.viewProducts)
+route.get('/visualizarUm/:nome', Product.viewOneProduct)
 
 module.exports = route
