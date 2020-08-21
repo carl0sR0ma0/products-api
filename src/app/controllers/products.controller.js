a76132108a42a5591a63d136d933e2e2c8c37694
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-21 01:20:12 -0300

    Implementando o visualizar products no controller de products.

--- src/app/controllers/products.controller.js
+++ src/app/controllers/products.controller.js
@@ -7,9 +7,19 @@ class Product {
 
     productSchema.create(body, (err, data) => {
       if (err) {
-        res.status(500).send({message: 'Houve um erro ao processar a sua requisição', error: err})
+        res.status(500).send({message: 'Houve um erro ao processar a sua requisição', error: err })
       } else {
-        res.status(201).send({message: 'Produto criado com sucesso no banco de dados', product: data})
+        res.status(201).send({message: 'Produto criado com sucesso no banco de dados', product: data })
+      }
+    })
+  }
+
+  viewProducts(req, res) {
+    productSchema.find({}, (err, data) => {
+      if (err) {
+        res.status(500).send({message: 'Houve um erro ao processar a sua requisição', error: err })
+      } else {
+        res.status(200).send({message: 'Todos os filmes foram recuperados com sucesso', products: data })
       }
     })
   }
