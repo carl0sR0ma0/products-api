9c75754c8da098f887a4da30daad1b1ea9cefba4
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-21 23:43:17 -0300

    Disponiblizando a rota de visualizar um.

--- src/app/routes/categories.routes.js
+++ src/app/routes/categories.routes.js
@@ -4,5 +4,6 @@ const Category = require('./../controllers/categories.controller')
 
 route.post('/criar', Category.createCategory)
 route.get('/visualizarTodos', Category.viewCategories)
+route.get('/visualizarUm/:nome', Category.viewOneCategory)
 
 module.exports = route
