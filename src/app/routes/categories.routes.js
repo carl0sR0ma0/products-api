18ab41d11fb37c42083576c6a341583f4e5db937
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-22 00:24:05 -0300

    Disponibilizando a rota de apagarUm.

--- src/app/routes/categories.routes.js
+++ src/app/routes/categories.routes.js
@@ -6,5 +6,6 @@ route.post('/criar', Category.createCategory)
 route.get('/visualizarTodos', Category.viewCategories)
 route.get('/visualizarUm/:nome', Category.viewOneCategory)
 route.put('/atualizarUm/:nome', Category.updateOneCategory)
+route.delete('/apagarUm/:nome', Category.deleteOneCategory)
 
 module.exports = route
