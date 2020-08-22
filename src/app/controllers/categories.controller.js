06f87b4518c2062282fd51b4f9b808c1b8c148f9
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-21 23:42:58 -0300

    Implementação de visualizar uma categoria.

--- src/app/controllers/categories.controller.js
+++ src/app/controllers/categories.controller.js
@@ -24,6 +24,18 @@ class Category {
       }
     })
   }
+
+  viewOneCategory(req, res) {
+    const name = req.params.nome
+
+    categorySchema.find({name: name}, (err, data) => {
+      if (err) {
+        res.status(500).send({message: 'Houve um erro ao processar a sua requisição', error: err })
+      } else {
+        res.status(200).send({message: `Categoria ${name} foi recuperada com sucesso`, category: data })
+      }
+    })
+  }
 }
 
 module.exports = new Category()
